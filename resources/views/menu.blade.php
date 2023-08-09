fb598afb2388837067e91573d54b4086d313d625
Galih akbar maulana <Galihakbarmaulana8@gmail.com>
2023-08-09 08:40:51 +0700

    first commit
    Instal Laravel, Route, Slicing Template, Dynamic, Menu/Content

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,14 @@
+
+<head>
+    <title>CRUD Laravel</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/admin.css') }}">
+</head>
+<body>
+    <h2>Menu<h2>
+        <br>
+        <a href="{{ url('home') }}">Home</a> -
+        <a href="{{ url('about') }}">About</a> -
+        <a href="{{ url('contact') }}">Contact</a> 
+        <hr>
+</body>
+
